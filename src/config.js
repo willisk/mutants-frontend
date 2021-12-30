6fb5e9d8888ae4ba8737d5c5b3b9b8a1fa794f3c
willisk <kurtwillis@live.com>
2021-12-30 10:46:19 +0100

    redeploy

--- src/config.js
+++ src/config.js
@@ -18,10 +18,13 @@ export const mutantsContractConfig = {
 };
 
 export const config = {
-  NFTAddress: '0x46623964150429B8317FbDF07F982F472C659E47',
-  MutantsAddress: '0x912e32391eAF5b93F80FF1E957c52C911cFa4DC5',
-  SerumAddress: '0x466fbbb0f58368596bc7B138AEBdAD6c5B6012f2',
-  supportedChainIds: [80001],
+  // NFTAddress: '0x46623964150429B8317FbDF07F982F472C659E47',
+  // MutantsAddress: '0x912e32391eAF5b93F80FF1E957c52C911cFa4DC5',
+  // SerumAddress: '0x466fbbb0f58368596bc7B138AEBdAD6c5B6012f2',
+  NFTAddress: '0x8a9Dd644938471870fe1E6dFdDE6B75375A143cE',
+  MutantsAddress: '0x34330bE0c7e99759FbBb665d5a66e8f386803827',
+  SerumAddress: '0x5970e1a60e2134760aeF3a28EE2d9F824785F60d',
+  supportedChainIds: [4, 80001],
   validChainName: 'Mumbai',
 };
 
