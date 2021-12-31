62bdd103ad327c47b2edea05848fc9ea2c7e0d09
willisk <kurtwillis@live.com>
2021-12-31 19:07:17 +0100

    tmp

--- src/config.js
+++ src/config.js
@@ -21,9 +21,9 @@ export const config = {
   // NFTAddress: '0x46623964150429B8317FbDF07F982F472C659E47',
   // MutantsAddress: '0x912e32391eAF5b93F80FF1E957c52C911cFa4DC5',
   // SerumAddress: '0x466fbbb0f58368596bc7B138AEBdAD6c5B6012f2',
-  NFTAddress: '0x09f7b9c205e6bc1c6B6518547Ae4AA150004C58F',
-  MutantsAddress: '0x7c0c67c4F47a95E52e375012fe6d370E52C057bA',
-  SerumAddress: '0x2Ff3533e0c13c7036e934C4C0c045b3e43f5b1B0',
+  NFTAddress: '0xd933ea64aC85D0f6D14B8FB484a7205B0B9C4834',
+  MutantsAddress: '0x6CdaC7439cDCda1b87DC803f3D36A6C21BA9BEe1',
+  SerumAddress: '0x8d237eca8B3D1548434f3CF863E06c98fB7d306c',
   supportedChainIds: [4],
   validChainName: 'Rinkeby',
 };
